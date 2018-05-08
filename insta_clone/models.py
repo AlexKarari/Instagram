666b5f1558a93b1b02829fb15c4d9bf4c7c0bc6e
alexkarari <k.alexander@gmail.com>
2018-05-08 17:16:38 +0300

    Models
    Class models created.

--- insta_clone/models.py
+++ insta_clone/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='profilepicture/', blank=True)
+    bio = models.CharField(max_length=1000)
+    user = models.ForeignKey(User)
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=1000)
+    
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='picfolder/')
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=100)
+    profile = models.ForeignKey(Profile)
+    likes = models.IntegerField(default=0)
+    comments = models.ForeignKey(Comments)
+    date_uploaded = models.DateTimeField(auto_now_add=True, blank=True)
+
+
