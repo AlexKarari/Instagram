23819d5fd161af44633b14039029d7b3bf8076c4
alexkarari <k.alexander@gmail.com>
2018-05-09 16:09:06 +0300

    views.py
    function for search created

--- insta_clone/models.py
+++ insta_clone/models.py
@@ -4,9 +4,30 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
     avatar = models.ImageField(upload_to='profilepicture/', blank=True)
     bio = models.TextField()
-    user = models.ForeignKey(User)
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profile(cls):
+        profile = cls.objects.all()
+        return profile
+    @classmethod
+    def search_profile(cls, search_term):
+        profile = cls.objects.filter(user_icontains=search_term)
+        return profile
+    
 
 class Comments(models.Model):
     comment = models.CharField(max_length=1000)
--- insta_clone/views.py
+++ insta_clone/views.py
@@ -5,3 +5,16 @@ from .models import Profile, Comments, Image
 def profile(request):
     prof_pic = Profile.objects.all()
     return render(request, 'all/profile.html', {'avatar': prof_pic})
+
+#View function to search for users in the app
+def search_results(request):
+    if 'user' in request.GET and request.GET["user"]:
+        search_term = request.GET.get("user")
+        searched_articles = Profile.search_by_user_username(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all/search.html', {"message": message, "articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all/search.html', {"message": message})
