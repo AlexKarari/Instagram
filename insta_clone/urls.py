e60fe5415263301f4d63624d29ffcdd8911e1270
alexkarari <k.alexander@gmail.com>
2018-05-09 16:35:05 +0300

    Url patterns
    New route that references the location of uploaded files created on the app's url.py

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -5,5 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$', views.profile, name='home'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
