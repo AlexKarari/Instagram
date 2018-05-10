7e7825144dcc9ee2f7e78408d517816fd4878c50
alexkarari <k.alexander@gmail.com>
2018-05-10 16:47:59 +0300

    App's views.py
    NNew routes for timeline and profile created

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -3,16 +3,23 @@ from .models import Profile, Comments, Image
 from django.contrib.auth.decorators import login_required
 
 # View Function to display the timeline
+@login_required(login_url='/accounts/login/')
 def timeline(request):
     current_user = request.user
     images = Image.objects.order_by('-date_uploaded')
-    
+    profile = Profile.objects.order_by('-date_created')
+    comment = Comments.objects.order_by('-comment_time')
+    return render(request, 'all/timeline.html', {'image':images, 'name':profile, 'comment':comment})
+
 
 # View Function to display a user's profile
 @login_required(login_url='/accounts/login/')
 def profile(request):
-    prof_pic = Profile.objects.all()
-    return render(request, 'all/profile.html', {'avatar': prof_pic})
+    current_user = request.user
+    prof_pic = Image.objects.get(user_id=current_user.id)
+    profile = Profile.objects.get(user_id=current_user.id)
+    prof_images = Image.objects.all().filter(profile_id=current_user.id)
+    return render(request, 'all/profile.html', {'avatar': prof_pic, 'name':profile, 'image':prof_images})
 
 #View function to search for users in the app
 def search_results(request):
