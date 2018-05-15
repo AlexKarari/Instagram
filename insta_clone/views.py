0056cd51f09c58db04915a27340d98c6cf35ca0e
alexkarari <k.alexander@gmail.com>
2018-05-15 17:05:42 +0300

    Search Functionality
    Search now working

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -47,10 +47,10 @@ def my_profile(request):
 def search_results(request):
     if 'user' in request.GET and request.GET["user"]:
         search_term = request.GET.get("user")
-        searched_articles = Profile.search_profile(search_term)
+        profiles = Profile.search_profile(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all/search.html', {"message": message, "articles": searched_articles})
+        return render(request, 'all/search.html', {"message": message, "profiles": profiles})
 
     else:
         message = "You haven't searched for any user"
