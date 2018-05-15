24025e13d6f333ae8bd96fa69f9f30893d81e5fd
alexkarari <k.alexander@gmail.com>
2018-05-15 11:58:38 +0300

    Account activation template
    message and link for activation created

--- insta_clone/views.py
+++ insta_clone/views.py
@@ -4,8 +4,13 @@ from .models import Profile, Comments, Image
 from django.db import transaction
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
-from .forms import NewCommentForm, NewImageForm, EditProfileForm, EditUserForm
+from .forms import NewCommentForm, NewImageForm, EditProfileForm, EditUserForm, SignUpForm
 from django.contrib.auth.models import User
+from mysite.core.tokens import account_activation_token
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 # View Function to display the timeline
 @login_required(login_url='/accounts/login/')
@@ -133,3 +138,25 @@ def like(request):
         return HttpResponseRedirect(request.META['HTTP_REFERER'], {"user_liked": user_liked})
 
     return HttpResponseRedirect('/')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your MySite Account'
+            message = render_to_string('account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect('account_activation_sent')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
